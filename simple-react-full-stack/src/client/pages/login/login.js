118f61c7808bc3b3ddcd3ec05220386230eee78f
skylermt <smt013@ucsd.edu>
2018-12-07 20:02:49 -0800

    working on getting authentication working in firebase

--- simple-react-full-stack/src/client/pages/login/login.js
+++ simple-react-full-stack/src/client/pages/login/login.js
@@ -1,46 +1,90 @@
 import React, { Component } from 'react';
-import { form, FormGroup, ControlLabel, FormControl } from 'react-bootstrap'
+import { form, FormGroup, ControlLabel, FormControl, Button} from 'react-bootstrap'
+
+const firebase = require('../../Firebase.js');
+
 export default class Login extends Component {
 	constructor(props) {
 
 		super(props)
 
 		this.state = {
-			username: '',
-			password: ''
+			email: "",
+			password: ""
 		}
+		//this.validateForm = this.validateForm.bind(this)
 	}
 
-	handleUsername (e) {
-		this.setState({
-			username : e.target.value
+	handleEmail = (e) => {
+		this.setState ({
+			email: e.target.value
 		})
 	}
 
-	handlePassword (e) {
-		this.setState({
-			password : e.target.value
+	handlePassword = (e) => {
+		this.setState ({
+			password: e.target.value
 		})
 	}
+
+	handleSubmit = (e) => {
+		this.validateForm(this.state.email,this.state.password)
+	}
+	
+	validateForm = (email,password) => {
+		debugger
+		console.log(email)
+		console.log(password)
+		firebase.default.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+			// Handle Errors here.
+  			var errorCode = error.code;
+			var errorMessage = error.message;
+			console.log(errorCode + errorMessage)
+		})
+		var user = firebase.default.auth().currentUser;
+
+			if (user) {
+			  // User is signed in.
+			} else {
+			  // No user is signed in.
+			}
+		}
 
 	render() {
 		return (
+			/*<div>
+				<form onSubmit={this.handleSubmit}>
+
+					<FormGroup controlId="email" bsSize="large">
+						<ControlLabel> Username </ControlLabel>
+						<FormControl autoFocus type="text" value={this.state.email} onChange={() => {this.handleEmail}}/>
+					</FormGroup>
+
+					<FormGroup controlId="password" bsSize="large">
+						<ControlLabel> Password </ControlLabel>
+						<FormControl type="text" value={this.state.password} onChange={() => {this.handlePassword}} type="password"/>
+					</FormGroup>
+
+					<Button block bsSize="large" disabled={this.validateForm()} type="submit"> Login </Button>
+
+				</form>
+			</div>*/
+
+			//<FieldGroup id="formControlsPassword" type="text" label="Password" placeholder="Password"/>
 			<div>
 				<h1> Login </h1>
-				<form>
+				<form onSubmit={this.handleSubmit}>
 					<FormGroup controlId="formBasicText">
 
-						<ControlLabel> Username </ControlLabel>
+						<ControlLabel> Email </ControlLabel>
 						<FormControl
 							type="text"
 							value={this.state.value}
-							placeholder="username"
-							onChange={this.handleUsername}
+							placeholder="email"
+							onChange={this.handleEmail}
 						/>
 					</FormGroup>
-				</form>
 
-				<form>
 					<FormGroup controlId="formBasicText">
 
 						<ControlLabel> Password </ControlLabel>
@@ -51,6 +95,7 @@ export default class Login extends Component {
 							onChange={this.handlePassword}
 						/>
 					</FormGroup>
+					<Button block bsSize="large" type="submit"> Login </Button>
 				</form>
 			</div>
 			)
